df86e205d85b8be133dd59cecf610eb6da4466f3
lankydonkey <alphakinetic@gmail.com>
2016-10-13 15:58:08 +0100

    ba

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return '<h1>Hello World again11111wefefd!</h1>'
+    return render_template("ProductSearch.html")
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
